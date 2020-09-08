1b39e93d78cb3d7cc0f8fedefabcc8d969927892
ViitzyzZ <braiantomasi0018@outlook.com>
2020-09-08 17:05:55 -0300

    feat exercicio-lp-05

---
+++ exercicio05.py
@@ -0,0 +1,7 @@
+#Jogos
+Listadejogos = ["Rainbow Six","Fifa","Forza Horizon","Gta"]
+print("Minha lista de jogos possuí: ", Listadejogos)
+if "Cod Black Ops" in Listadejogos:
+    print("Você possuí este jogo em sua lista.")
+else:
+    print("Você não possuí este jogo em sua lista.")
