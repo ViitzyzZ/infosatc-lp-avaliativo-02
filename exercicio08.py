9ac68dc4de54f41bffc28e53d0cec7fb7bcc0afe
ViitzyzZ <braiantomasi0018@outlook.com>
2020-09-08 17:46:41 -0300

    feat exercicio-lp-08

---
+++ exercicio08.py
@@ -0,0 +1,6 @@
+#Lista desorganizada de números inteiros
+Listadenumerosinteiros = [4, 9 , 2 , 5 , 3 , 7 , 18 , 0]
+print("A lista desorganizada possui tais números inteiros: ",Listadenumerosinteiros)
+#Lista ordenada
+Listadenumerosinteiros.sort()
+print(Listadenumerosinteiros)
