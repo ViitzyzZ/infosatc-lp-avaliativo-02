08c3274bfd3e5974a69c00d991261656fb043a27
ViitzyzZ <braiantomasi0018@outlook.com>
2020-09-08 18:09:00 -0300

    feat exercicio-lp-07

---
+++ exercicio07.py
@@ -0,0 +1,9 @@
+#Lista da compras de peças para o pc gamer
+Listadecompras = ["GeForce RTX 2080Ti", "Cooler master", "Placa Mãe MSI", "Monitor Acer"]
+print("A Lista de compras possui: ", Listadecompras)
+
+Listadecompras.remove("GeForce RTX 2080Ti")
+print("Lista com item removido: ",Listadecompras)
+
+Listadecompras.remove("Cooler master")
+print("Lista com item removido: ",Listadecompras)
