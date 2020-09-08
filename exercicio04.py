0a3ea3b6e94ed9302f18c73cbdc05c2d9340962f
ViitzyzZ <braiantomasi0018@outlook.com>
2020-09-08 18:17:05 -0300

    feat exercicio-lp-04

---
+++ exercicio04.py
@@ -0,0 +1,5 @@
+Listadejogos1 = ["Rainbow Six", "Fifa", "Forza Horizon", "Gta"]
+print("Minha Lista de jogos: ",Listadejogos1)
+
+Listadejogos2 = Listadejogos1,copy()
+print("Listadejogos2 que copiou a Listadejogos1: ",Listadejogos2)
