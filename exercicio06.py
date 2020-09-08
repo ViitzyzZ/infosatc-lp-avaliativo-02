f5a027ffbdf02572bd31b9cff73ad7dbadcb1991
ViitzyzZ <braiantomasi0018@outlook.com>
2020-09-08 17:27:03 -0300

    feat exercicio-lp-06

---
+++ exercicio06.py
@@ -0,0 +1,15 @@
+#Minhas quatro listas
+Listadejogos = ["Rainbow Six", "Fifa","Forza Horizon"]
+print("Minha lista de jogos possui: ",Listadejogos)
+Listadeseries = ["Arrow", "Peaky Blinders","The Walking Dead"]
+print("Minha lista de séries recomendada possui as seguinteds séries: ",Listadeseries)
+Listadecomidaspreferidas = ["Pizza", "Hambúrguer", "Lasanha"]
+print("Minha lista de comidas preferidas é: ", Listadecomidaspreferidas)
+Listadehobbies = ["Jogar Video Game","Jogar Futebol","Sair com os amigos","Dar rolê de moto"]
+print("Minha lista de hobbies possui: ", Listadehobbies)
+
+Listacompleta = Listadejogos + Listadeseries + Listadecomidaspreferidas + Listadehobbies
+print("A lista completa de todas as listas acima é: ",Listacompleta)
+
+#Acesso da Lista de séries
+print("Minha série favorita é: ",Listadeseries[0])
